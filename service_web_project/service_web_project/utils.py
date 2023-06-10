d507e96b028090307378e39473d4304a9d5453a9
Nathan Moussiron <75442379+MoussironNathan@users.noreply.github.com>
2023-06-10 22:16:38 +0200

    update

--- service_web_project/service_web_project/utils.py
+++ service_web_project/service_web_project/utils.py
@@ -2,6 +2,6 @@ import pymongo
 
 
 def get_db_handle():
-    # connect_string = 'mongodb+srv://<user>:<password>@servicewebdb.gyy9vjg.mongodb.net/'
+    connect_string = 'mongodb+srv://<user>:<password>@servicewebdb.gyy9vjg.mongodb.net/'
     client = pymongo.MongoClient(connect_string)
     return client
